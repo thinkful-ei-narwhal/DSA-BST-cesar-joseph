c8ee049795819ba4aeb3f4339e6b0190245f9cd1
Cesar Morales <cesarlenin2@live.com>
2020-06-16 08:36:36 -0700

    number 1 done

---
+++ bst-drills.js
@@ -0,0 +1,15 @@
+//1. Draw a BST
+//A:           3
+//          1      4
+//            2       6
+//                  5    9
+//                      7
+
+//B.           E
+//       A            S
+//        E      Q        Y
+//             I   S     U 
+//              O       T
+//             N
+
+//2. Remove the root
